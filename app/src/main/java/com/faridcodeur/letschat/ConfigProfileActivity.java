e825939f945d450cd6edee3e5779cee608c4f232
Farid AMOUSSA <amoussa.farid2@gmail.com>
2021-12-18 11:19:05 +0100

    error in res

---
+++ app/src/main/java/com/faridcodeur/letschat/ConfigProfileActivity.java
@@ -0,0 +1,14 @@
+package com.faridcodeur.letschat;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ConfigProfileActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_config_profile);
+    }
+}
