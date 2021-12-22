90bead107778828cb94d8ea79d2cc8b1c4c4fd1b
Amir SOULE <alabi@gmail.com>
2021-12-22 11:16:01 +0100

    profile

--- app/src/main/java/com/faridcodeur/letschat/MainActivity.java
+++ app/src/main/java/com/faridcodeur/letschat/MainActivity.java
@@ -3,12 +3,34 @@ package com.faridcodeur.letschat;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+    private Button btnClick;
+    private EditText Name, Date;
+    private TextView msg, NameOut, DateOut;
+
+    public void onClick(View v)
+    {
+        if (v == btnClick)
+        {
+            if (Name.equals("") == false )
+            {
+                NameOut = Name;
+                msg.setVisibility(View.VISIBLE);
+            }
+            else
+            {
+                msg.setText("Please complete both fields");
+
+            }
+        }
+        return;
+
     }
 }
--- app/src/main/java/com/faridcodeur/letschat/ProfileActivity.java
+++ app/src/main/java/com/faridcodeur/letschat/ProfileActivity.java
@@ -1,4 +1,10 @@
 package com.faridcodeur.letschat;
 
+import android.widget.Button;
+
+import androidx.activity.R;
+
 public class ProfileActivity {
+    private Button button;
+
 }
