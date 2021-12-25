b2243c588493ec1d45459112cb0dec03ee56b5d9
luc <luckdina45@gmail.com>
2021-12-26 00:21:56 +0100

    new

--- app/src/main/java/com/faridcodeur/letschat/MainActivity.java
+++ app/src/main/java/com/faridcodeur/letschat/MainActivity.java
@@ -2,13 +2,25 @@ package com.faridcodeur.letschat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import com.faridcodeur.letschat.databinding.ActivityMainBinding;
+
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.click.setOnClickListener(view -> {
+            Intent i = new Intent(MainActivity.this , Sondagebox.class);
+            startActivity(i);
+        });
     }
 }
